c9bf7beb8d45dd38724d5a0ab2abcf477e0369d1
Kevin Scannell <kscanne@gmail.com>
2016-09-09 08:18:47 -0500

    finishing touches

--- lab01/readcsv.py
+++ lab01/readcsv.py
@@ -3,7 +3,8 @@ import csv
 with open('tweets.csv', 'rb') as f:
   reader = csv.DictReader(f, delimiter=',')
   for row in reader:
-    lati = row['latitude']
-    longi = row['longitude']
-    count = row['tweets']
-    print lati, type(lati)
+    lati = float(row['latitude'])
+    longi = float(row['longitude'])
+    count = int(row['tweets'])
+    if lati > 51 and lati < 54 and longi < -2 and longi > -10:
+      print lati, longi
